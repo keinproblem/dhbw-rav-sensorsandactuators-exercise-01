166b956b1c53fbdd540750050e5b67fd1e15f743
keinproblem <noli.mo@icloud.com>
2018-06-04 17:21:34 +0200

    initial commit

---
+++ app/src/test/java/sensorsandactuators/ravensburg/dhbw/de/dhbw_rav_sensorsandactuators_exercise_01/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package sensorsandactuators.ravensburg.dhbw.de.dhbw_rav_sensorsandactuators_exercise_01;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
